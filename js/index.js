8e4a20fbbd8f298882a381454e8c8ad7c5b570a9
wantonmeeman <tokyo.yu.96@gmail.com>
2021-11-28 20:20:50 +0800

    Segemented Me page commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,29 @@
 $(document).ready(function () {
+
+    let imageArray = [
+        {
+            title: `TestTitle1`,
+            subTitle: `testSubTitle1`,
+            source: `https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg`
+        },
+        {
+            title: `TestTitle2`,
+            subTitle: `testSubTitle2`,
+            source: `https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg`
+        }
+    ]
+
+    let skillArray = [
+        {
+            title: `Python`,
+            proficiency: 4
+        },
+        {
+            title: `JavaScript`,
+            proficiency: 3
+        },
+    ]
+
     $('.nav-link').mouseenter(
         (x) => {
 
@@ -13,6 +38,7 @@ $(document).ready(function () {
                 }, 300)
                 // console.log(x.target.id)
             });
+
     $('.navbar-nav li a').click(function () {
         var clickedID = $(this).attr('id');
         if ($(this).hasClass('inactive')) { //this is the start of our condition 
@@ -22,8 +48,39 @@ $(document).ready(function () {
             $(this).addClass(`active`)
 
             $('.container').hide();
-            $('#' + clickedID.slice(0,clickedID.length - 4) + 'Content').fadeIn('slow')
+            $('#' + clickedID.slice(0, clickedID.length - 4) + 'Content').fadeIn('slow')
         }
     });
-    $('#leftImageGalleryContainer')
+
+    function returnProficiencyHTML(proficiency){
+        let returnString = ``;
+        for(var x = 0;proficiency > x;x++){
+            returnString += `<div class="bar col-md-1"></div>`
+        }
+        return returnString;
+    }
+    
+    for (let x = 0; skillArray.length > x; x++) {
+        $('#rightSkillContainer').append(`
+        <div class="skillRow row d-flex my-3 justify-content-end">
+          <div class="skillTitle col-md-5">
+            ${skillArray[x].title}
+          </div>
+          <div class="skillBar row col-md-7">
+            ${returnProficiencyHTML(skillArray[x].proficiency)}
+          </div>
+        </div>`)
+    }
+
+    for (let x = 0; imageArray.length > x; x++) {
+        $('#carouselContent').append(`
+            <div class="carousel-item ${x == 0 ? "active" : ""}">
+                <img src="${imageArray[x].source}" class="d-block w-100" alt="..." />
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>${imageArray[x].title}</h5>
+                    <p>${imageArray[x].subTitle}</p>
+                </div>
+            </div>`)
+    }
+
 });
